2de67fcfc59ead532dd6be2c37691a2cad4733ff
limon105-dev <limonali105@gmail.com>
2022-10-18 22:35:49 +0600

    akori personal portfolio

---
+++ js/script.js
@@ -0,0 +1,181 @@
+'use strict'
+// ------about swiper js start ------//
+var swiper = new Swiper(".mySwiper_about", {
+    direction: "vertical",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});;
+
+// ------about swiper js end ------//
+
+// ------mask swiper js start ------//
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+// ------mask swiper js end ------//
+
+
+// ------back-to-top button js start ------//
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// ------back-to-top button js end ------//
+
+// ------ portfolio filter js start ------//
+var Shuffle = window.Shuffle;
+
+class Demo {
+    constructor(element) {
+        this.element = element;
+        this.shuffle = new Shuffle(element, {
+            itemSelector: '.picture-item',
+            sizer: element.querySelector('.my-sizer-element'),
+            isRTL: true,
+            isCentered: true
+        });
+
+        // Log events.
+        this.addShuffleEventListeners();
+        this._activeFilters = [];
+        this.addFilterButtons();
+        this.addSorting();
+    }
+
+    /**
+     * Shuffle uses the CustomEvent constructor to dispatch events. You can listen
+     * for them like you normally would (with jQuery for example).
+     */
+    addShuffleEventListeners() {
+        this.shuffle.on(Shuffle.EventType.LAYOUT, (data) => {
+            console.log('layout. data:', data);
+        });
+        this.shuffle.on(Shuffle.EventType.REMOVED, (data) => {
+            console.log('removed. data:', data);
+        });
+    }
+
+    addFilterButtons() {
+        const options = document.querySelector('.filter-options');
+        if (!options) {
+            return;
+        }
+
+        const filterButtons = Array.from(options.children);
+        const onClick = this._handleFilterClick.bind(this);
+        filterButtons.forEach((button) => {
+            button.addEventListener('click', onClick, false);
+        });
+    }
+
+    _handleFilterClick(evt) {
+        const btn = evt.currentTarget;
+        const isActive = btn.classList.contains('active');
+        const btnGroup = btn.getAttribute('data-group');
+
+        this._removeActiveClassFromChildren(btn.parentNode);
+
+        let filterGroup;
+        if (isActive) {
+            btn.classList.remove('active');
+            filterGroup = Shuffle.ALL_ITEMS;
+        } else {
+            btn.classList.add('active');
+            filterGroup = btnGroup;
+        }
+
+        this.shuffle.filter(filterGroup);
+    }
+
+    _removeActiveClassFromChildren(parent) {
+        const {
+            children
+        } = parent;
+        for (let i = children.length - 1; i >= 0; i--) {
+            children[i].classList.remove('active');
+        }
+    }
+
+    addSorting() {
+        const buttonGroup = document.querySelector('.sort-options');
+        if (!buttonGroup) {
+            return;
+        }
+        buttonGroup.addEventListener('change', this._handleSortChange.bind(this));
+    }
+
+    _handleSortChange(evt) {
+        // Add and remove `active` class from buttons.
+        const buttons = Array.from(evt.currentTarget.children);
+        buttons.forEach((button) => {
+            if (button.querySelector('input').value === evt.target.value) {
+                button.classList.add('active');
+            } else {
+                button.classList.remove('active');
+            }
+        });
+    }
+
+    /**
+     * Filter the shuffle instance by items with a title that matches the search input.
+     * @param {Event} evt Event object.
+     */
+    _handleSearchKeyup(evt) {
+        const searchText = evt.target.value.toLowerCase();
+        this.shuffle.filter((element, shuffle) => {
+            // If there is a current filter applied, ignore elements that don't match it.
+            if (shuffle.group !== Shuffle.ALL_ITEMS) {
+                // Get the item's groups.
+                const groups = JSON.parse(element.getAttribute('data-groups'));
+                const isElementInCurrentGroup = groups.indexOf(shuffle.group) !== -1;
+                // Only search elements in the current group
+                if (!isElementInCurrentGroup) {
+                    return false;
+                }
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.demo = new Demo(document.getElementById('grid'));
+});
+
+// ------ portfolio filter js end ------//
